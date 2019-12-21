f8596876fa6c7d80c33c7b2b940a517a1127f3a4
xgqfrms <xgqfrms@outlook.com>
2019-12-21 23:44:25 +0800

    hello world (c/cpp/cs)

---
+++ 000-xyz/hello-world.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    // C 程序
+    printf("Hello, World! \n");
+    return 0;
+}
---
+++ 000-xyz/hello-world.cs
@@ -0,0 +1,14 @@
+using System;
+
+// namespace
+namespace HelloWorldNameSpace {
+    // 类名
+    class HelloWorld {
+        // main 函数
+        static void Main(string[] args) {
+            // C# 程序
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
